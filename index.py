3256d9275848969df1b33d78f82d0f3418fa7bee
Eric Vargas <ericvargas2004@gmail.com>
2025-11-08 12:16:05 -0500

    Added linear, ridge and lasso regression models

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
 from sklearn.metrics import mean_squared_error, r2_score, davies_bouldin_score
 
 y_pred = None #predicted values placeholder
@@ -23,7 +24,7 @@ print(edf.head()) #sanity check
 
 # Split the data into training and testing sets
 
-X = edf.iloc[:, edf.columns != 'price'] #training data columns
+X = edf.drop(columns=['price', 'flight']) #training data columns
 y = edf.iloc[:,4]
 
 X_train, X_test, y_train, y_test = train_test_split(
@@ -35,18 +36,39 @@ print(y_test.head())
 print(y_train.head())
 
 
+# Linear Regression Model
+print("Training linear regression model...")
+lin_reg = LinearRegression()
+lin_reg.fit(X_train, y_train)
+y_pred_lin = lin_reg.predict(X_test)
+print("Done training linear regression.")
+
+# Ridge Regression Model
+print("Training ridge regression regression model...")
+ridge_reg = Ridge(alpha=1) 
+ridge_reg.fit(X_train, y_train)
+y_pred_ridge = ridge_reg.predict(X_test)
+print("Done training ridge regression.")
+
+# Lasso Regression Model: needs scaled data for faster weight convergences
+print("Training lasso regression model...")
+lasso_reg = Lasso(alpha=0.01)
+lasso_reg.fit(X_train, y_train)
+y_pred_lasso = lasso_reg.predict(X_test)
+print("Done lasso linear regression.")
+
 # Metric Analysis
 
 # Linear Regression Metrics: RMSE, R2 Score
 try:
-    mse = mean_squared_error(y_true, y_pred) #mean squared error calculation using y_true and y_pred
+    mse = mean_squared_error(y_test, y_pred) #mean squared error calculation using y_true and y_pred
     rmse = np.sqrt(mse) #root mean squared error conversion
     print("\nRMSE: ", rmse)
 except Exception as e:
     print("\nError calculating MSE/RMSE: ", e)
 
 try:
-    r2 = r2_score(y_true, y_pred) #r2 score calculation using y_true and y_pred
+    r2 = r2_score(y_test, y_pred) #r2 score calculation using y_true and y_pred
     print("\nR2 Score: ", r2)
 except Exception as e:
     print("\nError calculating R2 Score: ", e)
